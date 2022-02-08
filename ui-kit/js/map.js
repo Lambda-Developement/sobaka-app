d122c144c741733a467938b8a8115c2758906573
huscker <egostalker03@mail.ru>
2022-02-08 16:49:33 +0300

    Добавил карту на экран с картой

--- ui-kit/js/map.js
+++ ui-kit/js/map.js
@@ -1,8 +1,8 @@
 var HEIGHT = window.innerHeight+'px';
-var pos = [57.615398, 39.885228];
-var scale = 13;
-var locs = [[57.620188, 39.898177,1],
-            [57.619867, 39.879848,2]];
+const pos = [57.621398, 39.880228];
+var scale = 15;
+var locs = [[57.620188, 39.898177],
+            [57.619867, 39.879848]];
 
 document.getElementById("map").style.height = HEIGHT;
 
@@ -11,8 +11,6 @@ var map = L.map('map', {
     zoom:scale,
     zoomControl: false,
 })
-
-map = null;
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -25,6 +23,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 map.on('move',redraw);
 map.on('scale',redraw);
 
+redraw()
 
 function zoomin(){
     map.zoomIn(1);
@@ -35,14 +34,24 @@ function zoomout(){
 function redraw(){
     // console.log(map.getCenter());
     //console.log(map.getBounds());
-    pos = [map.getCenter().lat,map.getCenter().lng];
-    document.getElementById('icons-here').innerHTML = "";
+    let pos_x = map.latLngToContainerPoint(L.latLng(pos[0],pos[1])).x;
+    let pos_y = map.latLngToContainerPoint(L.latLng(pos[0],pos[1])).y;
+    document.getElementById('icons-here').innerHTML = "" +
+        "<div style=\"top: "+pos_y+"px;left: "+pos_x+"px\" class=\"my-position d-flex justify-content-center align-items-center\">\n" +
+        "            <div class=\"orange-circle\"></div>\n" +
+        "        </div>";
     locs.forEach((a)=>{
-        console.log(map.latLngToContainerPoint(L.latLng(a[0],a[1])));
         let x = map.latLngToContainerPoint(L.latLng(a[0],a[1])).x;
         let y = map.latLngToContainerPoint(L.latLng(a[0],a[1])).y;
         if(0 <= x && x <= window.innerWidth && 0 <= y && y <= window.innerHeight){
-
+            document.getElementById('icons-here').innerHTML+="" +
+                "<div style=\"top:"+y+"px;left:"+x+"px;\" class=\"position-fixed\">\n" +
+                "            <a href=\"#\" onclick=\"collapse_toggle()\">\n" +
+                "                <div class=\"geopoint d-flex justify-content-center align-items-center pb-2 \">\n" +
+                "                    <div class=\"dog-img\"></div>\n" +
+                "                </div>\n" +
+                "            </a>\n" +
+                "        </div>"
         }
     })
 }
