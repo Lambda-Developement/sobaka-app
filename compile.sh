d279e3ed8d6898da88a4b8ca5de9a31ac99edb27
Ilya Tampio <quakumei@gmail.com>
2022-04-25 19:49:20 +0300

    wip

---
+++ compile.sh
@@ -0,0 +1,1 @@
+cordova plugin rm cordova-plugin-deeplinks; cordova plugin rm cordova-universal-links-plugin; cordova platform rm android; cordova platform add android; cordova plugin add cordova-universal-links-plugin; cordova plugin rm cordova-universal-links-plugin;  cordova plugin add https://github.com/walteram/cordova-universal-links-plugin 
